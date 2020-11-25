259ffd6685d7b2b3f944dd6b413099e7ca3ca4b8
Raaid Mahboob <raaidsm@gmail.com>
2020-11-25 04:06:14 -0500

    Implement basic template for Reading, Customer, TOUCustomer and TIERCustomer

--- main.cpp
+++ main.cpp
@@ -5,36 +5,66 @@ using namespace std;
 
 class Reading {
 private:
-    long meterNumber;
+    int meterNumber;
     unsigned char time;
+
+public:
+    Reading(int meterNumber, unsigned char time) : meterNumber(meterNumber), time(time) {}
+    ~Reading() = default;
 };
 
 class Customer {
-private:
-    long meterNumber;
+protected:
+    int meterNumber;
     list<Reading> readings;
-    long totalKwhUsed;
-    float balance;
+    int totalKwhUsed;
+    double balance;
 
 public:
-    void addReading() {}
-    void setBalance() {}
-    [[nodiscard]] float getBalance() const {
+    Customer() {
+        meterNumber = 0;
+        totalKwhUsed = 0;
+        balance = 0;
+    }
+    Customer(int newMeterNumber, int newTotalKwhUsed, double newBalance) {
+        meterNumber = newMeterNumber;
+        totalKwhUsed = newTotalKwhUsed;
+        balance = newBalance;
+    }
+    virtual ~Customer() = default;
+
+    void addReading(Reading reading) {
+        readings.push_back(reading);
+    }
+    void setBalance(double newBalance) {
+        balance = newBalance;
+    }
+    [[nodiscard]] double getBalance() const {
         return balance;
     }
+    virtual void computeBalance() = 0;
 };
 
 class TOUCustomer : public Customer {
+public:
+    TOUCustomer() = default;
+    TOUCustomer(int newMeterNumber, int newTotalKwhUsed, double newBalance) : Customer(
+            newMeterNumber, newTotalKwhUsed, newBalance) {}
 
+    void computeBalance() final {}
 };
 
 class TIERCustomer : public Customer {
+public:
+    TIERCustomer() = default;
+    TIERCustomer(int newMeterNumber, int newTotalKwhUsed, double newBalance) : Customer(
+            newMeterNumber, newTotalKwhUsed, newBalance) {}
 
+    void computeBalance() final {}
 };
 
-
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Customer* cust = new TOUCustomer;
+    cust->computeBalance();
     return 0;
 }
